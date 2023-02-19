3086b844910b5f116dd3f2166421a050beff53b7
grossmann4 <grossmann4@yahoo.com>
2023-02-19 12:33:39 -0500

    testing downloading webpages

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ def main():
     # df = doc_freq(tf_list) # dict
     # print(df)
 
-    link = output[7]['url']
+    link = output[8]['url']
     try:
         htmlfile = urllib.request.urlopen(link).read()
     except urllib.error.HTTPError as e:
