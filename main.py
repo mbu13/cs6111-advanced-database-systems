82e23620a631af0044420adcebfbd87c68879ba1
grossmann4 <grossmann4@yahoo.com>
2023-02-19 14:44:22 -0500

    finished getting list of possible query words

--- main.py
+++ main.py
@@ -31,26 +31,46 @@ def get_formatted_items(items):
         return {'title': item['title'], 'url': item['link'], 'description': item['snippet']}
     return [get_attr(i) for i in items]
 
+"""
+    Function to get the relevant webpages from the 10 googled webpages
+
+    input: list of webpages
+    output: list of webpages relevant to query 
+"""
 def get_relevant_docs(output):
+    # initialize return variables
     precision = 0
     relevant = []
-    nr = []
+
+    # iterate over each google webpage
     for i, doc in enumerate(output):
+        # print webpage summary and prompt user for relevance feedback
         print(json.dumps(doc, indent=4))
         rel = input("Relevant (Y/N)? ")
+
+        # make sure user input is in correct format
         while rel != "Y"  and rel != "N":
             rel = input("Relevant (Y/N)? Only type Y or N ")
+        # add webpage to relevant list if user inputs "Y"
         if rel == 'Y':
             precision += 1
             relevant.append(output[i])
-        elif rel == 'N':
-            nr.append(output[i])
-        
-    return precision/10, relevant, nr
+    
+    return precision/10, relevant
 
+"""
+    Function to compute the term frequency for each relevant webpage
+
+    input: webpage text (string), list of stop words
+    output: dictionary of term frequencies 
+"""
 def word_frequency(text, stop):
+    # initialize dictionary
     tflist = {}
+    # split webpage text into seperate words
     doc = text.lower().split("\n")
+
+    # for each word in the webpage, increment the term frequency
     for word in doc:
         if word not in stop:
             if word not in tflist:
@@ -58,8 +78,17 @@ def word_frequency(text, stop):
             tflist[word] += 1
     return tflist
 
+"""
+    Function to get the document frequency for each term in the relevane webpages
+
+    input: list of term frequency dictionaries
+    output: dictionary of document frequencies 
+"""
 def doc_freq(tf_list):
+    # initialize document frequency dictionary
     df = {}
+
+    # increment each word in the document once if it appears in the document
     for result in tf_list:
         for key in result:
             if key not in df:
@@ -67,49 +96,91 @@ def doc_freq(tf_list):
             df[key] += 1
     return df
 
+"""
+    Function to get the tf-idf scores for each document
+
+    input: list term freqency dictionaries, dictionary of document frequencies,
+           number of relevant documents
+    output: list of tf-idf dictionaries 
+"""
 def tf_idf(tf_list, df, N):
     score_list = tf_list
+
+    # for each term in the document, compute the tf-idf score
     for i, result in enumerate(score_list):
         for key in result:
             score_list[i][key] = (1 + math.log(result[key], 10)) * (math.log(N/df[key],10))
 
     return score_list
 
+"""
+    Function to get the term frequencies from the body of each webpage
+
+    input: list of webpages, list of stop words
+    output: list of term frequency dictionaries
+"""
 def get_website(output, stop):
+    # initialize return variable
     tf_list = []
+
+    # for each webpage
     for i, doc in enumerate(output):
+        # get the webpage url
         link = doc['url']
+        # try to open the website, go to the next webpage if error is given
         try:
             htmlfile = urllib.request.urlopen(link).read()
         except urllib.error.HTTPError as e:
             continue
+        # parse the webpage for the body
         soup = bs4.BeautifulSoup(htmlfile, features="html.parser").find('body')
         for script in soup(["script", "style"]):
             script.extract()
-
         text = soup.get_text()
         lines = (line.strip() for line in text.splitlines())
         chunks = (phrase.strip() for line in lines for phrase in line.split(" "))
         text = '\n'.join(chunk for chunk in chunks if chunk)
+
+        # calculate term frequecies of the webpage body
         tf = word_frequency(text, stop)
+        # add term frequency dictionary to list
         tf_list.append(tf)
     return tf_list
 
-def get_maxes(tfidf, query,df):
+"""
+    Function to get the best new querying terms
+
+    input: list of term frequency dictionaries, initial query,
+           document frequency dictionary
+    output: sorted list of new query words 
+"""
+def get_maxes(tfidf, query, df):
+    # initialize return variable
     keys = []
+
+    # for each webpage
     for lis in tfidf:
+        # initialize counter variable
         count = 0
+        # sort dictionary by term frequency greatest to least
         lis = sorted(lis.items(), key=lambda kv: (kv[1], kv[0]))
         lis.reverse()
+        # for each term in the dictionary
         for tup in lis:
+            # get the term with the highest term frequency
             k = str(tup[0])
+            # get the term frequency
             v = tup[1]
+            # check to see if the term is already in the query
             if k not in query:
+                # add term to list
                 keys.append(tuple((k,v, df[k])))
+                # make sure each document provides at most 5 possible terms
                 count += 1
                 if count > 4:
                     break
-
+    
+    # sort the list of terms by document frequency
     sort = sorted(keys, key=lambda tup: tup[2])
     sort.reverse()
     return sort
@@ -137,7 +208,7 @@ def main():
         exit("")
 
     # prompt user for relevance feedback
-    precision, relevant, not_relevant = get_relevant_docs(output)
+    precision, relevant = get_relevant_docs(output)
 
     # check if first ten documents are good enough
     if precision >= PRECISION:
@@ -146,6 +217,7 @@ def main():
             "\nDesired precision reached, done")
         exit("")
 
+    # check if all documents are not relevant
     if precision == 0:
         print("======================\n"
             + "Query: " + str(WORDS) + "\nPrecision: " + str(precision) + 
@@ -154,11 +226,21 @@ def main():
     
     # get the word frequency for each document
     tf_list = get_website(relevant, stop) # list of dicts
+    # check if none of the webpages could be analyzed
+    if len(tf_list) == 0:
+        print("======================\n"
+            + "Query: " + str(WORDS) + "\nPrecision: " + str(precision) + 
+            "\nNone of the webpages could be analyzed, done")
+        exit("")
+
     # get the document frequency for each word
     df = doc_freq(tf_list) # dict
+
     # tfidf = tf_idf(tf_list, df, len(tf_list))
-    keys = get_maxes(tf_list, str(WORDS).lower(), df)
-    print(keys)
+
+    # get sorted list of potential words to query on (sorted based on df)
+    keys = get_maxes(tf_list, str(WORDS).lower(), df) # list of tuple(word, tf, df)
+    #print(keys)
     
     
 
