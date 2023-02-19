db72bd8dbbd5a77eed7955e3b91468dd0fc92349
grossmann4 <grossmann4@yahoo.com>
2023-02-18 19:15:26 -0500

    testing downloading webpages

--- main.py
+++ main.py
@@ -57,7 +57,14 @@ def main():
     link = output[0]['url']
     htmlfile = urllib.request.urlopen(link).read()
     soup = bs4.BeautifulSoup(htmlfile, features="html.parser").find('body')
-    print(soup)
+    for script in soup(["script", "style"]):
+        script.extract()
+
+    text = soup.get_text()
+    lines = (line.strip() for line in text.splitlines())
+    chunks = (phrase.strip() for line in lines for phrase in line.split(" "))
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+    print(text)
 
 if __name__ == "__main__":
     main()
