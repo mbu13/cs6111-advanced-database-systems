43d5107710749a0713d54ca8f5686a9b28297f92
grossmann4 <grossmann4@yahoo.com>
2023-04-22 17:40:51 -0400

    scaffolding

--- project3/main.py
+++ project3/main.py
@@ -1,11 +1,21 @@
 import sys
-import json
+import pandas as pd
+import pandasql as sql
+
+def one_item_sets(DF):
+
+    return None
+
+def apriori(L1, DF):
+    return None
 
 def main():
-    if len(sys.argv) < 5:
+    # get program arguments and check values
+    if len(sys.argv) < 4:
         print('Required input format: python3 main.py INTEGRATED-DATASET.csv <min_sup> <min_conf>')
         return
     DATASET = sys.argv[1]
+    print(DATASET)
     MIN_SUP = float(sys.argv[2])
     MIN_CONF = float(sys.argv[3])
 
@@ -16,8 +26,12 @@ def main():
     if MIN_CONF < 0 or MIN_CONF > 1:
         print("min_sup must be between 0 and 1")
         return
+    # open dataset as a pandas dataframe
+    DF = pd.read_csv(DATASET)
     
-    
+    L1 = one_item_sets(DF)
+    ANSWER = apriori(L1, DF)
+
 
 
 if __name__ == "__main__":
